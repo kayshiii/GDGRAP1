54616f112b09ecb58b3864cd8fddd900fb03abb8
kayshiii <116062496+kayshiii@users.noreply.github.com>
2023-05-22 17:27:17 +0800

    test

---
+++ week1/Shaders/sample.frag
@@ -0,0 +1,8 @@
+#version 330 core
+
+out vec4 FragColor;
+
+void main() {
+//                r     g     b     a
+FragColor = vec4(0.4f, 0.0f, 0.0f, 1.0f);
+}
--- week1/week1.cpp
+++ week1/week1.cpp
@@ -19,8 +19,38 @@
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
 
-
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <math.h>
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h"
+
+#include <string>
+
+float x_mod = 0;
+float y_mod = 0;
+
+void Key_CallBack(
+    GLFWwindow* window,
+    int key,
+    int scancode,
+    int action, //press/release
+    int mod
+) {
+    if (key == GLFW_KEY_D && action == GLFW_PRESS) {
+        x_mod += 0.1f;
+    }
+    if (key == GLFW_KEY_A && action == GLFW_PRESS) {
+        x_mod -= 0.1f;
+    }
+    if (key == GLFW_KEY_W && action == GLFW_PRESS) {
+        y_mod += 0.1f;
+    }
+    if (key == GLFW_KEY_S && action == GLFW_PRESS) {
+        y_mod -= 0.1f;
+    }
+}
 
 int main(void)
 {
@@ -41,13 +71,154 @@ int main(void)
     }
 
     /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    //glfwMakeContextCurrent(window);
     //end of initialization
+
+    /* make the windows context current */
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+
+    glfwSetKeyCallback(window, Key_CallBack);
+
+    std::fstream vertSrc("Shaders/sample.vert");
+    std::stringstream vertBuff;
+    vertBuff << vertSrc.rdbuf();
+    std::string vertS = vertBuff.str();
+    const char* v = vertS.c_str();
+
+    std::fstream fragSrc("Shaders/sample.frag");
+    std::stringstream fragBuff;
+    fragBuff << fragSrc.rdbuf();
+    std::string fragS = fragBuff.str();
+    const char* f = fragS.c_str();
+
+    GLuint vertShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertShader, 1, &v, NULL);
+    glCompileShader(vertShader);
+
+    GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragShader, 1, &f, NULL);
+    glCompileShader(fragShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertShader);
+    glAttachShader(shaderProgram, fragShader);
+
+    glLinkProgram(shaderProgram);
+
+    std::string path = "3D/bunny.obj";
+    std::vector<tinyobj::shape_t> shape;
+    std::vector<tinyobj::material_t> material;
+    std::string warning, error;
+
+    tinyobj::attrib_t attributes;
+
+    bool success = tinyobj::LoadObj(
+        &attributes,
+        &shape,
+        &material,
+        &warning,
+        &error,
+        path.c_str()
+    );
+
+    std::vector<GLuint> mesh_indices;
+    for (int i = 0; i < shape[0].mesh.indices.size(); i++) {
+        mesh_indices.push_back(
+            shape[0].mesh.indices[i].vertex_index
+        );
+    }
+
+    GLfloat vertices[]{
+        // x  y  z
+        0.f, 0.5, 0.f, //0
+        -0.5f, -0.5f, 0.f, //1
+        0.5f, -0.5, 0.f//2
+    };
+
+    GLuint indices[]{
+        0, 1, 2
+    };
+
+    GLuint VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    //were working with this VAO
+    glBindVertexArray(VAO);
+    //were working with this VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        sizeof(GLfloat) * attributes.vertices.size(),
+        &attributes.vertices[0],
+        GL_STATIC_DRAW
+        //GL_DYNAMIC_DRAW
+    );
+
+    glVertexAttribPointer(
+        0,
+        3, //XYZ
+        GL_FLOAT,
+        GL_FALSE,
+        3 * sizeof(GLfloat),
+        (void*)0
+    );
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(
+        GL_ELEMENT_ARRAY_BUFFER,
+        sizeof(GLuint) * mesh_indices.size(),
+        mesh_indices.data(),
+        GL_STATIC_DRAW
+    );
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //x_mod += 0.001f;
+
+        unsigned int xLoc = glGetUniformLocation(shaderProgram, "x");
+        glUniform1f(xLoc, x_mod);
+
+        unsigned int yLoc = glGetUniformLocation(shaderProgram, "y");
+        glUniform1f(yLoc, y_mod);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(
+            GL_TRIANGLES,
+            mesh_indices.size(),
+            GL_UNSIGNED_INT,
+            0
+        );
+
+        /* swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    /* Loop until the user closes the window */
+    /*
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        /*
         glClear(GL_COLOR_BUFFER_BIT);
 
         glBegin(GL_TRIANGLES); //top
@@ -67,13 +238,14 @@ int main(void)
         glVertex2f(-0.5f, 0.f);
         glVertex2f(0.5f, 0.f);
         glEnd();
+        */
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        //glfwSwapBuffers(window);
 
         /* Poll for and process events */
-        glfwPollEvents();
-    }
+        //glfwPollEvents();
+    //}
 
     glfwTerminate();
     return 0;
